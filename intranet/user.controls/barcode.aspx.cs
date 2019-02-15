abd14c879c9bba53043fc586a110e23defaf7420
jmcota <jmcota@ontica.org>
2019-02-14 21:28:47 -0500

    Fix Barcode component must dispose rendering resources

--- intranet/user.controls/barcode.aspx.cs
+++ intranet/user.controls/barcode.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Drawing.Imaging;
 
 using C1.Win.C1BarCode;
 
@@ -7,43 +8,46 @@ namespace Empiria.Web.UI {
 
   public partial class BarCodeControl : System.Web.UI.Page {
 
-    private void Page_Load(object sender, System.EventArgs e) {
+    private void Page_Load(object sender, EventArgs e) {
       Response.Clear();
       Response.ContentType = "image/gif";
       PrintBarcode();
       Response.End();
     }
 
+
     private Image GetBarcode() {
-      C1BarCode bc = new C1BarCode();
-      bc.CodeType = CodeTypeEnum.Code128;
-      bc.Text = Request.QueryString["data"];
+      using (var bc = new C1BarCode()) {
 
-      int height = int.Parse(Request.QueryString["height"] ?? "-1");
-      if (height > 0) {
-        bc.BarHeight = height;
+        bc.CodeType = CodeTypeEnum.Code128;
+        bc.Text = Request.QueryString["data"];
+
+        int height = int.Parse(Request.QueryString["height"] ?? "-1");
+        if (height > 0) {
+          bc.BarHeight = height;
+        }
+
+        bool showText = bool.Parse(Request.QueryString["show-text"] ?? "false");
+        bc.ShowText = showText;
+
+        return bc.GetImage(ImageFormat.Gif);
       }
-
-      bool showText = bool.Parse(Request.QueryString["show-text"] ?? "false");
-      bc.ShowText = showText;
-
-      //return bc.Image;
-      return bc.GetImage(System.Drawing.Imaging.ImageFormat.Gif);
     }
+
 
     private void PrintBarcode() {
-      Image barcodeImage = this.GetBarcode();
-
       bool showVertical = bool.Parse(Request.QueryString["vertical"] ?? "false");
 
-      if (showVertical) {
-        barcodeImage.RotateFlip(RotateFlipType.Rotate270FlipNone);
-      }
+      using (Image barcodeImage = this.GetBarcode()) {
+        if (showVertical) {
+          barcodeImage.RotateFlip(RotateFlipType.Rotate270FlipNone);
+        }
 
-      barcodeImage.Save(Response.OutputStream, System.Drawing.Imaging.ImageFormat.Gif);
+        barcodeImage.Save(Response.OutputStream, ImageFormat.Gif);
+      }
 
     }
 
-  } // class CalendarControl
+  } // class BarCodeControl
 
 } // namespace Empiria.Web.UI
