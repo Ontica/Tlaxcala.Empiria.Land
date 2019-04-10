e0ac951a7cb0f3411535eb98d30abb2784f635ef
jmcota <jmcota@ontica.org>
2019-04-09 23:52:18 -0400

    Add digitalization.editor aspx web page

---
+++ intranet/land.registration.system.transactions/digitalization.editor.aspx
@@ -0,0 +1,146 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" Inherits="Empiria.Land.WebApp.DigitalizationEditor" Codebehind="digitalization.editor.aspx.cs" %>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+  <title>Digitalización de trámites</title>
+  <meta http-equiv="Expires" content="-1" />
+  <meta http-equiv="Pragma" content="no-cache" />
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <link href="../themes/default/css/secondary.master.page.css" type="text/css" rel="stylesheet" />
+  <link href="../themes/default/css/editor.css" type="text/css" rel="stylesheet" />
+  <script type="text/javascript" src="../scripts/empiria.ajax.js"></script>
+  <script type="text/javascript" src="../scripts/empiria.general.js"></script>
+  <script type="text/javascript" src="../scripts/empiria.secondary.master.page.js"></script>
+  <script type="text/javascript" src="../scripts/empiria.validation.js"></script>
+</head>
+<body style="background-color:#fafafa; top:0; margin:0; margin-top:-14px; margin-left:-6px;">
+  <form name="aspnetForm" method="post" enctype="multipart/form-data" id="aspnetForm" runat="server">
+    <div id="divContentAlwaysVisible">
+
+      <table id="tabStripItemView_0" style="display:inline;">
+        <tr>
+          <td class="subTitle">Documentos digitalizados asociados al trámite</td>
+        </tr>
+
+       <tr>
+        <td style="width:100%">
+          <div style="overflow:auto;width:100%;">
+            <table class="details" style="width:97%">
+              <tr class="detailsHeader">
+                <td style="white-space:nowrap">Tipo de documento</td>
+                <td style="white-space:nowrap">Digitalizado por</td>
+                <td style="white-space:nowrap">Fecha</td>
+                <td style="white-space:nowrap;width:40%">&nbsp;</td>
+              </tr>
+              <%=base.GetDocuments()%>
+            </table>
+          </div>
+          <% if (base.CanEditDocuments()) { %>
+          <div>
+            <br /><br />
+            <table>
+              <% if (!base.documentSet.HasMainDocument) { %>
+              <tr>
+                <td>Documento principal:</td>
+                <td><input type="file" id="mainDocument" name="mainDocument" style="width:400px" accept="application/pdf" runat="server" /></td>
+              </tr>
+              <% } %>
+              <% if (!base.documentSet.HasAuxiliaryDocument) { %>
+              <tr>
+                <td>Anexos:</td>
+                <td><input type="file" id="auxiliaryDocument" name="auxiliaryDocument" style="width:400px" accept="application/pdf" runat="server" /></td>
+              </tr>
+              <% } %>
+              <% if (!base.documentSet.HasMainDocument || !base.documentSet.HasAuxiliaryDocument) { %>
+              <tr>
+                <td>&nbsp;</td>
+                <td><input class="button" type="button" value="Subir archivos" onclick="doOperation('updloadFiles')" style="height:28px;width:128px" /></td>
+              </tr>
+              <% } %>
+            </table>
+          </div>
+          <% } %>
+        </td>
+      </tr>
+
+     </table>
+
+    </div>
+  </form>
+</body>
+
+<script type="text/javascript">
+
+  function doOperation(command) {
+    var success = false;
+
+    if (gbSended) {
+      return;
+    }
+    switch (command) {
+      case "updloadFiles":
+        updloadFiles();
+        return;
+
+      case "viewFile":
+        viewFile(arguments[1]);
+        return;
+
+      case "deleteFile":
+        deleteFile(arguments[1]);
+        return;
+
+      case "refresh":
+        refresh();
+        return;
+
+      default:
+        alert("La operación '" + command + "' no ha sido definida en el programa.");
+        return;
+    }
+    if (success) {
+      sendPageCommand(command);
+      gbSended = true;
+    }
+  }
+
+
+  function viewFile(url) {
+    createNewWindow(url);
+  }
+
+  function deleteFile(id) {
+    var msg = "¿Elimino el archivo seleccionado?";
+
+    if (confirm(msg)) {
+      sendPageCommand("deleteFile", "documentId=" + id);
+    }
+  }
+
+  function updloadFiles() {
+    sendPageCommand("updloadFiles");
+  }
+
+  function refresh() {
+    window.location.reload();
+  }
+
+  function updateUserInterface(oControl) {
+    if (oControl == null) {
+      return;
+    }
+  }
+
+  function window_onload() {
+    <%=base.OnLoadScript%>
+  }
+
+  function window_onunload() {
+
+  }
+
+  addEvent(window, 'load', window_onload);
+  addEvent(window, 'unload', window_onunload);
+
+</script>
+</html>
---
+++ intranet/land.registration.system.transactions/digitalization.editor.aspx.cs
@@ -0,0 +1,231 @@
+﻿/* Empiria Land **********************************************************************************************
+*                                                                                                            *
+*  Module   : Land Intranet                                Component : Transaction UI                        *
+*  Assembly : Empiria.Land.WebApp.dll                      Pattern   : Web Editor                            *
+*  Type     : DigitalizationEditor                         License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Transaction documents digitalization editor.                                                   *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Web;
+
+using Empiria.Presentation.Web;
+
+using Empiria.Land.Documentation;
+using Empiria.Land.Registration.Transactions;
+
+namespace Empiria.Land.WebApp {
+
+  /// <summary>Transaction documents digitalization editor.</summary>
+  public partial class DigitalizationEditor : WebPage {
+
+    #region Fields
+
+    protected LRSTransaction transaction = null;
+    protected TransactionDocumentSet documentSet = null;
+
+    protected string OnLoadScript = String.Empty;
+
+    #endregion Fields
+
+    #region Constructors and parsers
+
+    protected void Page_Load(object sender, EventArgs e) {
+      Initialize();
+      if (!IsPostBack) {
+        LoadEditor();
+      } else {
+        ExecuteCommand();
+      }
+    }
+
+    #endregion Constructors and parsers
+
+    #region Public methods
+
+
+    protected bool CanEditDocuments() {
+      return true;
+    }
+
+
+    protected string GetDocuments() {
+      string template = "<tr class={{CLASS}}>" +
+                          "<td style='white-space:nowrap'><a href=\"javascript:doOperation('viewFile', '../{{FILE-URL}}')\">" +
+                                "<img src='../themes/default/buttons/pdf.png'>{{DOCUMENT-TYPE}}</a></td>" +
+                          "<td style='white-space:nowrap'>{{DIGITALIZED-BY}}</td>" +
+                          "<td style='white-space:nowrap'>{{DATE}}</td>" +
+                          "<td style='width:40%'>{{OPTIONS}}</td>" +
+                        "</tr>";
+      string result = String.Empty;
+
+      string temp = String.Empty;
+
+      if (documentSet.HasMainDocument) {
+        temp = template.Replace("{{CLASS}}", "detailsItem");
+        temp = temp.Replace("{{DOCUMENT-TYPE}}", "Documento principal");
+        temp = temp.Replace("{{FILE-URL}}", documentSet.MainDocument.UrlRelativePath);
+        temp = temp.Replace("{{DIGITALIZED-BY}}", documentSet.MainDocument.DigitalizedBy.Alias);
+        temp = temp.Replace("{{DATE}}", documentSet.MainDocument.DigitalizationDate.ToString("dd/MMM/yyyy HH:mm"));
+        temp = temp.Replace("{{OPTIONS}}", GetDeleteLink(documentSet.MainDocument));
+        result = temp;
+      }
+
+      if (documentSet.HasAuxiliaryDocument) {
+        temp = template.Replace("{{CLASS}}", "detailsOddItem");
+        temp = temp.Replace("{{DOCUMENT-TYPE}}", "Anexos");
+        temp = temp.Replace("{{FILE-URL}}", documentSet.AuxiliaryDocument.UrlRelativePath);
+        temp = temp.Replace("{{DIGITALIZED-BY}}", documentSet.AuxiliaryDocument.DigitalizedBy.Alias);
+        temp = temp.Replace("{{DATE}}", documentSet.AuxiliaryDocument.DigitalizationDate.ToString("dd/MMM/yyyy HH:mm"));
+        temp = temp.Replace("{{OPTIONS}}", GetDeleteLink(documentSet.AuxiliaryDocument));
+        result += temp;
+      }
+
+      if (result.Length == 0) {
+        result = "<tr><td colspan='4'>Aún no se han digitalizado los documentos de este trámite.</td></tr>";
+      }
+
+      return result;
+    }
+
+
+    static private string GetDeleteLink(TransactionDocument document) {
+      const string template =
+        "<a href='javascript:doOperation(\"deleteFile\", {{ID}});' title='Elimina este documento digitalizado'>" +
+                "<img src='../themes/default/buttons/trash.gif'></a>";
+
+      return template.Replace("{{ID}}", document.Id.ToString());
+    }
+
+
+    #endregion Public methods
+
+    #region Private methods
+
+    private void DeleteFile() {
+      int id = int.Parse(GetCommandParameter("documentId"));
+
+      if (documentSet.MainDocument.Id == id) {
+        documentSet.MainDocument.Delete();
+      } else if (documentSet.AuxiliaryDocument.Id == id) {
+        documentSet.AuxiliaryDocument.Delete();
+      }
+    }
+
+
+    private void Refresh() {
+      Response.Redirect("digitalization.editor.aspx?transactionId=" + transaction.Id.ToString(), true);
+    }
+
+
+    private void UploadFiles() {
+      Assertion.AssertObject(this.transaction, "Transaction is null.");
+      Assertion.Assert(!this.transaction.IsEmptyInstance, "Transaction can't be the empty instance.");
+
+      UploadMainFile();
+      UploadAuxiliaryFile();
+    }
+
+
+    private void UploadMainFile() {
+      HttpPostedFile uploadedFile = Request.Files["mainDocument"];
+
+      if (!ValidFile(uploadedFile, "Documento principal")) {
+        return;
+      }
+
+      DocumentUploader.SetMainDocument(this.transaction, uploadedFile);
+
+      uploadedFile.InputStream.Dispose();
+    }
+
+
+    private void UploadAuxiliaryFile() {
+      HttpPostedFile uploadedFile = Request.Files["auxiliaryDocument"];
+
+      if (!ValidFile(uploadedFile, "Archivo de anexos")) {
+        return;
+      }
+
+      DocumentUploader.SetAuxiliaryDocument(this.transaction, uploadedFile);
+
+      uploadedFile.InputStream.Dispose();
+    }
+
+
+    private bool ValidFile(HttpPostedFile uploadedFile, string documentType) {
+      if (uploadedFile == null) {
+        return false;
+      }
+
+      if (String.IsNullOrWhiteSpace(uploadedFile.FileName)) {
+        return false;
+      }
+
+      if (uploadedFile.ContentLength == 0) {
+        SetMessageBox($"El {documentType} está vacío.");
+        return false;
+      }
+
+      if (uploadedFile.ContentType != "application/pdf") {
+        SetMessageBox($"El {documentType} no está en formato PDF.");
+        return false;
+      }
+
+      return true;
+    }
+
+
+    private void ExecuteCommand() {
+      switch (base.CommandName) {
+        case "updloadFiles":
+          UploadFiles();
+          Refresh();
+          return;
+
+        case "deleteFile":
+          DeleteFile();
+          Refresh();
+          return;
+
+        case "refresh":
+          Refresh();
+          return;
+
+        default:
+          throw new NotImplementedException(base.CommandName);
+      }
+    }
+
+
+    private void Initialize() {
+      int transactionId = int.Parse(Request.QueryString["transactionId"]);
+
+      if (transactionId != 0) {
+        transaction = LRSTransaction.Parse(transactionId);
+      } else {
+        transaction = LRSTransaction.Empty;
+      }
+      documentSet = TransactionDocumentSet.ParseFor(this.transaction);
+    }
+
+
+    private void LoadEditor() {
+
+    }
+
+    private void SetMessageBox(string msg) {
+      OnLoadScript += "alert('" + msg + "');";
+    }
+
+
+    private void SetRefreshPageScript() {
+      OnLoadScript += "sendPageCommand('redirectMe');";
+    }
+
+    #endregion Private methods
+
+  } // class DigitalizationEditor
+
+} // namespace Empiria.Land.WebApp
