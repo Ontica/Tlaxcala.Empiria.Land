2a471dc02a9590867e7cd7ac6677887c88e8bf83
jmcota <jmcota@ontica.org>
2019-09-05 22:17:57 -0500

    Add an aggregate web api project including Onepoint and Land web apis

---
+++ web.api/Empiria.Land.Tlaxcala.WebApi.AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿/* Empiria Land **********************************************************************************************
+*                                                                                                            *
+*  Module   : Empiria Land Tlaxcala Edition              Component : Web Api interface                       *
+*  Assembly : Empiria.Land.Tlaxcala.WebApi.dll           Pattern   : Assembly Attributes File                *
+*  Type     : None                                       License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Aglutinador de servicios web de tipo Http/Json para el RPP del Estado de Tlaxcala.             *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Reflection;
+
+/*************************************************************************************************************
+* Assembly configuration attributes.                                                                         *
+*************************************************************************************************************/
+[assembly: AssemblyTrademark("Empiria and Ontica are either trademarks of La Vía Óntica SC or Ontica LLC.")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
---
+++ web.api/README.md
@@ -0,0 +1,11 @@
+﻿# Empiria Land Web Api Tlaxcala Edition
+
+Aglutinador de servicios web de tipo Http/Json para el Registro Público de la Propiedad del Estado de Tlaxcala.
+
+# License
+
+This software solution is distributed by the GNU AFFERO GENERAL PUBLIC LICENSE.
+
+# Copyright
+
+Copyright © 2009-2019. La Vía Óntica, Ontica LLC and contributors.
