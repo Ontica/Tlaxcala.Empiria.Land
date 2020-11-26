4d9541d51361b3e2d4f5be9736457d507650cc64
José Manuel Cota <jmcota@ontica.org>
2020-11-25 23:09:47 -0500

    Customer-related cadastral web api moved from https://bit.ly/2V6LmtF

---
+++ web.api/CadastralServices/DocumentsController.cs
@@ -0,0 +1,50 @@
+﻿/* Empiria Land **********************************************************************************************
+*                                                                                                            *
+*  Solution  : Empiria Land                                     System   : Land Web API                      *
+*  Namespace : Empiria.Land.WebApi                              Assembly : Empiria.Land.WebApi.dll           *
+*  Type      : CertificatesController                           Pattern  : Web API                           *
+*  Version   : 3.0                                              License  : Please read license.txt file      *
+*                                                                                                            *
+*  Summary   : Web API used to read and edit land recording documents.                                       *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Web.Http;
+
+using Empiria.Data;
+
+using Empiria.WebApi;
+
+namespace Empiria.Land.WebApi {
+
+  /// <summary>Web API used to read and edit land recording documents.</summary>
+  public class DocumentsController : WebApiController {
+
+    #region Public APIs
+
+    [HttpGet, AllowAnonymous]
+    [Route("v1/documents/{documentUID}")]
+    public SingleObjectModel GetDocument(string documentUID) {
+      try {
+        base.RequireResource(documentUID, "documentUID");
+
+        string sql = "SELECT * FROM vwLRSCadastralWS WHERE CadastralKey = '{0}'";
+
+        var data = DataReader.GetDataRow(DataOperation.Parse(String.Format(sql, documentUID)));
+
+        if (data != null) {
+          return new SingleObjectModel(this.Request, data, "Empiria.Land.Property");
+        } else {
+          throw new ResourceNotFoundException("Document.UID",
+                    "Document with identifier '{0}' was not found.", documentUID);
+        }
+      } catch (Exception e) {
+        throw base.CreateHttpException(e);
+      }
+    }
+
+    #endregion Public APIs
+
+  }  // class DocumentsController
+
+}  // namespace Empiria.Land.WebApi
---
+++ web.api/CadastralServices/PropertyController.cs
@@ -0,0 +1,143 @@
+﻿/* Empiria Land **********************************************************************************************
+*                                                                                                            *
+*  Solution  : Empiria Land                                     System   : Land Web API                      *
+*  Namespace : Empiria.Land.WebApi                              Assembly : Empiria.Land.WebApi.dll           *
+*  Type      : PropertyController                               Pattern  : Web API                           *
+*  Version   : 3.0                                              License  : Please read license.txt file      *
+*                                                                                                            *
+*  Summary   : Contains services used to get real estate and other resources data.                           *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Web.Http;
+
+using Empiria.Data;
+using Empiria.WebApi;
+
+using Empiria.Land.Registration;
+
+namespace Empiria.Land.WebApi {
+
+  /// <summary>Contains services used to get real estate and other resources data.</summary>
+  public class PropertyController : WebApiController {
+
+    #region Public APIs
+
+    [HttpGet]
+    [AllowAnonymous]
+    [Route("v1/properties/{propertyUID}")]
+    public SingleObjectModel GetProperty(string propertyUID) {
+      try {
+        base.RequireResource(propertyUID, "propertyUID");
+
+        string sql = "SELECT * FROM LRSProperties WHERE PropertyUID = '{0}'";
+
+        var data = DataReader.GetDataRow(DataOperation.Parse(String.Format(sql, propertyUID)));
+
+        if (data != null) {
+          return new SingleObjectModel(this.Request, data, "Empiria.Land.Property");
+        } else {
+          throw this.PropertyNotFound(propertyUID);
+        }
+      } catch (Exception e) {
+        throw base.CreateHttpException(e);
+      }
+    }
+
+    /// <summary>Gets textual information about a registered property given its unique ID.</summary>
+    [HttpGet]
+    [Route("v1/properties/{propertyUID}/as-html")]
+    public SingleObjectModel GetPropertyTextInfo([FromUri] string propertyUID) {
+      try {
+        base.RequireResource(propertyUID, "propertyUID");
+
+        var property = RealEstate.TryParseWithUID(propertyUID);
+
+        if (property != null) {
+          return new SingleObjectModel(this.Request, this.GetPropertyAsTextModel(property),
+                                       "Empiria.Land.PropertyAsHtml");
+        } else {
+          throw this.PropertyNotFound(propertyUID);
+        }
+      } catch (Exception e) {
+        throw base.CreateHttpException(e);
+      }
+    }
+
+    [HttpGet]
+    [Route("v1/properties/{propertyUID}/antecedent")]
+    public SingleObjectModel GetPropertyAntecedent(string propertyUID) {
+      try {
+        base.RequireResource(propertyUID, "propertyUID");
+
+        var property = RealEstate.TryParseWithUID(propertyUID);
+
+        if (property == null) {
+          throw this.PropertyNotFound(propertyUID);
+        }
+
+        var antecedent = property.Tract.GetRecordingAntecedent();
+        var fullTract = property.Tract.GetRecordingActs();
+
+        var data = new {
+          antecedent = this.GetRecordingActModel(antecedent),
+          fullTract = fullTract.Select((x) => this.GetRecordingActModel(x)),
+        };
+
+        return new SingleObjectModel(this.Request, data, "Empiria.Land.PropertyAntecedents");
+      } catch (Exception e) {
+        throw base.CreateHttpException(e);
+      }
+    }
+
+    private object GetRecordingActModel(RecordingAct act) {
+      return new {
+        id = act.Id,
+        typeId = act.RecordingActType.Id,
+        type = act.RecordingActType.DisplayName,
+      };
+    }
+
+    [HttpGet, AllowAnonymous]
+    [Route("v1/properties/cadastral/{cadastralKey}")]
+    public SingleObjectModel GetPropertyWithCadastralKey(string cadastralKey) {
+      try {
+        base.RequireResource(cadastralKey, "cadastralKey");
+
+        string sql = "SELECT * FROM vwLRSCadastralWS WHERE CadastralKey = '{0}'";
+
+        var data = DataReader.GetDataRow(DataOperation.Parse(String.Format(sql, cadastralKey)));
+
+        if (data != null) {
+          return new SingleObjectModel(this.Request, data, "Empiria.Land.Property");
+        } else {
+          throw new ResourceNotFoundException("Property.CadastralKey",
+                "No tengo registrado ningún predio con la clave catastral '{0}'.", cadastralKey);
+        }
+      } catch (Exception e) {
+        throw base.CreateHttpException(e);
+      }
+    }
+
+    #endregion Public APIs
+
+    #region Private methods
+
+    private object GetPropertyAsTextModel(RealEstate o) {
+      return new {
+        uid = o.UID,
+        asHtml = "El folio electrónico del predio es <strong>" + o.UID  + "</strong>.<br/><br/>Este es el texto que debería " +
+                 "desplegarse en el <i>editor de trámites</i> CITyS para confirmar que se trata del mismo predio.",
+      };
+    }
+
+    private Exception PropertyNotFound(string propertyUID) {
+      return new ResourceNotFoundException("Property.UniqueID",
+          "No tengo registrado ningún predio con el folio real '{0}'.", propertyUID);
+    }
+
+    #endregion Private methods
+
+  }  // class PropertyController
+
+}  // namespace Empiria.Land.WebApi
